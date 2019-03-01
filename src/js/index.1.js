2b14a5ffa9fdd465e80430169538bccafbc8ef91
unknown <silaciok@gmail.com>
2019-03-01 17:28:04 +0200

    Add my homework

---
+++ src/js/index.1.js
@@ -0,0 +1,7 @@
+var age = 19;
+
+if(age > 25) {
+    document.write("<b>I am over 25 years old");
+} else {
+    document.write("<b>I am under 25");
+}
---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+var n = 18;
+console.log("my number: ", n);
+console.log("number has type: ", typeof n);
+
+var name = "Iura Dumitrascu";
+console.log("my name", name);
+console.log("name has type: ", typeof name);
+
+var YES = true;
+		var NO = false;
+
+		alert(YES);
+        alert(NO);
+        
+var carName = null;        
+
+var z;
+
+var y = 100;
+y = undefined;
+
+var user = { name: "Anton" };
